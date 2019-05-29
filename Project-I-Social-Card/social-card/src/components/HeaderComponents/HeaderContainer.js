42142a010dbccee37ecc60d682902b26b001c053
Roger-Lee <rmlee0003@gmail.com>
2019-05-29 12:21:48 -0400

    created headerContent,container,title components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,10 @@
 import React from 'react';
+import HeaderTitle from './HeaderTitle'
+
+import HeaderContent from './HeaderContent'
 import './Header.css';
+
+function HeaderContainer() {
+    return <div className="header-container"> <HeaderTitle /><HeaderContent /></div>;
+}
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,7 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle(){
+    return <h2>Lambda School</h2>
+}
+export default HeaderTitle;
