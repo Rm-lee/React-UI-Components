a56eb0a3ffdea0e4e3462bc75a2020f2490268bc
Roger Lee <rmlee0003@gmail.com>
2019-05-29 21:06:20 -0400

    added current date to headertitle from momemnt.js

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import moment from 'moment'
 import './Header.css';
 import HeaderContainer from './HeaderContainer';
 
 
-
+const time = moment().format("MMM Do YY");
 function HeaderTitle(){
-    return <p className="header-title"><strong>Lambda School</strong> @LambdaSchool &middot; 26 jan</p>
+    return <p className="header-title"><strong>Lambda School</strong> @LambdaSchool &middot; {time}</p>
 }
 export default HeaderTitle;
